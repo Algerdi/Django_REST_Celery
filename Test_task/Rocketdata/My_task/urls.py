0973f14663910a5e6d7a126660c4df47beb867e5
Algerdi <82950883+Algerdi@users.noreply.github.com>
2022-01-21 19:45:26 +0300

    Update urls.py

--- Test_task/Rocketdata/My_task/urls.py
+++ Test_task/Rocketdata/My_task/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('employee/add/', EmployeeCreateView.as_view()),   # Создание нового сотрудника
-    path('employee/all/', EmployeeListView.as_view()),   # Просмотр всех сотрудников для стаффа и только себя для обычного смертного
+    path('employee/all/', EmployeeListView.as_view()),   # Просмотр всех сотрудников для стаффа и только себя
     path('employee/single/<int:emp_id>/', EmployeeSingleListView.as_view()),   # Просмотр одного сотрудника
     path('employee/level/<int:level>/', EmployeeLevelListView.as_view()),   # Просмотр сотрудников по уровню
     path('employee/edit/<int:pk>/', EmployeeDetailView.as_view()),   # Редактирование данных сотрудника
